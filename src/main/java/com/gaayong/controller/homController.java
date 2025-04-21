570affa3d7b30b50bf8e6f7f64249f0f58440391
Jini <devjini82@gmail.com>
2025-04-21 17:17:18 +0900

    Feat: 대시보드 퍼블리싱 완료

---
+++ src/main/java/com/gaayong/controller/homController.java
@@ -0,0 +1,13 @@
+package com.gaayong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class homController {
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+}
