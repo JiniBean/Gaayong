9516a229df514b274dd1a62238021cc97932b2cd
Jini <devjini82@gmail.com>
2025-04-22 16:21:06 +0900

    Feat: SecurityConfig

---
+++ src/main/java/com/gaayong/config/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.gaayong.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        return encoder;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((request) -> request
+                        .anyRequest().permitAll());
+//                .formLogin()
+
+        return http.build();
+    }
+}
