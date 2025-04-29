4797e7b546d47a305eda7fc41d847bb40e006644
Jini <devjini82@gmail.com>
2025-04-29 15:41:03 +0900

    Feat: rememberme 설정

--- src/main/java/com/gaayong/config/security/SecurityConfig.java
+++ src/main/java/com/gaayong/config/security/SecurityConfig.java
@@ -8,15 +8,33 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 
     @Bean
@@ -36,12 +54,25 @@ public class SecurityConfig {
                         .usernameParameter("userNm")
                         .passwordParameter("pwd")
                         .permitAll())
+                .rememberMe(remember -> remember
+                        .key("uniqueAndSecretKey")
+                        .tokenRepository(tokenRepository())
+                        .tokenValiditySeconds(60 * 60 * 24 * 30) // 30일간 유효
+                        .rememberMeParameter("remember-me"))
                 .logout(logout -> logout
                         .logoutUrl("/signout")
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll());
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .permitAll())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .invalidSessionUrl("/signin?invalid=true")
+                        .sessionFixation().migrateSession()
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false)
+                        .expiredUrl("/signin?expired=true")
+                );
 
         return http.build();
     }
